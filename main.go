c9199dd284e1bb09a9b5a0a9c8f01d8ea9289f92
nocd5 <nocd5rd@gmail.com>
2018-05-09 19:34:33 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/koron/gomigemo/embedict"
+	"github.com/koron/gomigemo/migemo"
+	"os"
+	"regexp"
+	"runtime"
+	"sort"
+	"strings"
+	"sync"
+)
+
+var (
+	ignoreCaseOpt   = flag.Bool("i", false, "ignore case")
+	forwardMatchOpt = flag.Bool("f", false, "forward match")
+)
+
+func main() {
+	flag.Parse()
+	a := flag.Args()
+
+	if len(a) < 2 {
+		fmt.Fprintf(os.Stderr, "Invalid argments\n")
+		return
+	}
+
+	pat := a[0]
+	src := a[1:]
+
+	// Load embedded dictionary.
+	d, err := embedict.Load()
+	if err != nil {
+		panic(err)
+	}
+	// Get regex pattern with migemo
+	p, err := migemo.Pattern(d, pat)
+	if err != nil {
+		panic(err)
+	}
+
+	if *forwardMatchOpt {
+		p = "^" + p
+	}
+	if *ignoreCaseOpt {
+		p = "(?i)" + p
+	}
+	re, err := regexp.Compile(p)
+	if err != nil {
+		panic(err)
+	}
+
+	n := runtime.NumCPU()
+	s := make(chan bool, n)
+	q := make(chan string, 1)
+
+	var wg sync.WaitGroup
+	for _, e := range src {
+		wg.Add(1)
+		go func(_e string) {
+			s <- true
+			defer func() { <-s }()
+
+			if re.MatchString(_e) {
+				q <- _e
+			} else {
+				wg.Done()
+			}
+		}(e)
+	}
+
+	var l []string
+	go func() {
+		for t := range q {
+			l = append(l, t)
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
+	sort.Strings(l)
+	fmt.Println(strings.Join(l, "\n"))
+}
